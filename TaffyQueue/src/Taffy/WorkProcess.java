f80523b4dcf8db839cf09da9a3e6ac30956d4694
jesus <jesus@DESKTOP-87HEJON>
2022-12-02 14:20:13 +0900

    클래스 병합

--- TaffyQueue/src/Taffy/WorkProcess.java
+++ TaffyQueue/src/Taffy/WorkProcess.java
@@ -4,8 +4,8 @@ public class WorkProcess {
 	public static int Work(MakeQueue<Person> Person, MakeQueue<Person> Result, Person p1, int TotWaitTime,
 			int TotCusNum, int ServiceTime) {
 		for (int i = 1; i < 480; i++) {
-			IsWorkNow.Check(p1, ServiceTime);
-			TotCusNum = IsCusEnter.Check(Person, TotCusNum, i);
+			IsWorkNow(p1, ServiceTime);
+			TotCusNum = IsCusEnter(Person, TotCusNum, i);
 
 			if (ServiceTime > 0) { // 서비스타임이 주어졌을 때 업무를 수행
 				System.out.printf("고객 %d이 업무 처리중입니다.\n", p1.personNum);
@@ -27,5 +27,23 @@ public class WorkProcess {
 			}
 		}
 		return TotWaitTime;
+	}
+	
+	public static void IsWorkNow(Person p1, int i) {
+		System.out.printf("-----현재 시간 %d분-----\n", i);
+		if (p1.doService == true) { // 태피가 업무중인지 여부 검사
+			System.out.println("태피는 휴식 중\n");
+		} else {
+			System.out.println("태피는 작업 중\n");
+		}
+	}
+	
+	public static int IsCusEnter(MakeQueue<Person> Person, int TotCusNum, int i) {
+		if (((int) (Math.random() * 4) + 1) == 4) { // 난수가 4일 경우 손님 입장
+			Person per = new Person(i, TotCusNum++);
+			Person.enqueue(per);
+			System.out.printf("고객 %d이 %d분에 들어옵니다. 업무 처리시간 : %d\n", per.personNum, per.arriveTime, per.serviceTime);
+		}
+		return TotCusNum;
 	}
 }
